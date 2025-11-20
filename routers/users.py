b76cc97074e64503d67662a91af444f5648b21a4
yousef5200 <yassssoobekaso@gmail.com>
2025-11-20 15:43:37 +0200

    Add files via upload

--- routers/users.py
+++ routers/users.py
@@ -1,15 +1,23 @@
-from fastapi import APIRouter , HTTPException
 from models.user import UpdateUser ,Login,User,UserResponse,DeleteResponse
 from database import get_connection
-import mysql.connector as ms 
-from passlib.hash import bcrypt
-from typing import List
 from utils.hash import hash_password , verify_password
+from typing import List
+import mysql.connector as ms 
+from fastapi import APIRouter , HTTPException ,Depends
+from utils.jwt import create_access_token ,verify_access_token
+from fastapi.security import OAuth2PasswordBearer
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")  # الرابط الخاص بالـ login endpoint
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    payload = verify_access_token(token)
+    if payload is None:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    return payload
 
 router=APIRouter()
 @router.get("/users/",response_model=List[UserResponse])
-async def get_users():
+async def get_users(current_user: dict = Depends(get_current_user)):
     try:
         conn=get_connection()
         cursor=conn.cursor(dictionary=True)
@@ -25,14 +33,14 @@ async def get_users():
             conn.close()    
 
 @router.get("/users/{user_id}",response_model=UserResponse)
-async def get_user(user_id:int):
+async def get_user(user_id:int , current_user: dict = Depends(get_current_user)):
     try:
         conn=get_connection()
         cursor=conn.cursor(dictionary=True)
         cursor.execute("SELECT id,name,age,email FROM users WHERE id =%s",(user_id,))
         fetch=cursor.fetchone()
         if fetch is None:
-            raise HTTPException(status_code=401,detail="user not found")
+            raise HTTPException(status_code=404,detail="user not found")
         return fetch
     except ms.Error as e:
         raise HTTPException(status_code=500,detail=f"My SQL Error{str(e)}")
@@ -67,10 +75,12 @@ async def add_users(user:User):
             conn.close()    
 
 @router.put("/users/{user_id}",response_model=UserResponse)
-async def update_users(user_id:int , update_user:UpdateUser):
+async def update_users(user_id:int , update_user:UpdateUser ,current_user: dict = Depends(get_current_user)):
     try:
         conn=get_connection()
         cursor=conn.cursor()
+        if user_id != current_user["user_id"]:
+         raise HTTPException(status_code=403, detail="Not authorized")
         cursor.execute("UPDATE users SET name=%s , age=%s , email=%s WHERE id =%s",(update_user.name,update_user.age,update_user.email,user_id,))
         conn.commit()
         if cursor.rowcount == 0:
@@ -91,14 +101,18 @@ async def update_users(user_id:int , update_user:UpdateUser):
 
 
 @router.delete("/users/{user_id}",response_model=DeleteResponse)
-async def delete_users(user_id:int):
+async def delete_users(user_id:int,current_user: dict = Depends(get_current_user)):
     try:
         conn=get_connection()
         cursor=conn.cursor()
+        if user_id != current_user["user_id"]:
+         raise HTTPException(status_code=403, detail="Not authorized")
+
         cursor.execute("DELETE FROM users WHERE id =%s",(user_id,))
         conn.commit()
         if cursor.rowcount == 0:
             raise HTTPException(status_code=404,detail="user not found")
+       
         return DeleteResponse(
             message="deleted",
             user_id=user_id
@@ -111,28 +125,24 @@ async def delete_users(user_id:int):
         if "conn" in locals():
             conn.close()    
 
-@router.post("/login/" , response_model=UserResponse)
-async def login(login:Login) :
+@router.post("/login/")
+async def login(login: Login):
     try:
-        conn=get_connection()
-        cursor=conn.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM users WHERE Email =%s",(login.email,))
-        user=cursor.fetchone()
-        if user is None :
-            raise HTTPException(status_code=401, detail="wrong email or password")
-        if not verify_password(login.password , user["password_hash"]):
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM users WHERE Email = %s", (login.email,))
+        user = cursor.fetchone()
+        if user is None or not verify_password(login.password, user["password_hash"]):
             raise HTTPException(status_code=401, detail="wrong email or password")
 
-        return UserResponse(
-            id=user["id"],
-            name=user["name"],
-            age=user["age"],
-            email=user["Email"]
-        ) 
-    except ms.Error as e :
+        # إنشاء JWT token
+        access_token = create_access_token({"user_id": user["id"], "email": user["Email"]})
+        return {"access_token": access_token, "token_type": "bearer"}
+
+    except ms.Error as e:
         raise HTTPException(status_code=500, detail=f"MySQL Error: {str(e)}")
     finally:
         if "cursor" in locals():
             cursor.close()
         if "conn" in locals():
-            conn.close()                                                              
+            conn.close()
